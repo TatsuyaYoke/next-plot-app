80ebaa360834db291318fd6d36144088139030ac
Yoke <tayok@synspective.com>
2022-03-12 03:52:32 +0900

    add function of getting tlmNames

--- pages/api/fetch-tlm.js
+++ pages/api/fetch-tlm.js
@@ -19,7 +19,7 @@ const FetchTlm = async (_req, res) => {
         return new Promise((resolve, _reject) => {
             const db = new sqlite3.Database(path)
             db.serialize(() => {
-                db.all('select DATE, PCDU_BAT_VOLTAGE from test', (err, records) => {
+                db.all('select DATE, PCDU_BAT_VOLTAGE, PCDU_BAT_CURRENT from test', (err, records) => {
                     const data = toObjectArray(records)
                     resolve(data)
                 })
--- pages/plot/ground-test.js
+++ pages/plot/ground-test.js
@@ -1,19 +1,31 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Plot from 'react-plotly.js'
+import axios from 'axios'
 
 const GroundTest = () => {
-
     const [date, setDate] = useState([])
-    const [voltage, setVoltage] = useState([])
+    const [otherTlm, setOtherTlm] = useState({})
+    const [tlmNames, setTlmNames] = useState([])
+    const plotlyColors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52'] 
+    
+    const fetchTlmNames = async () => {
+        const response = await axios.get('http://localhost:3000/api/fetch-tlm')
+        const tlmNames = Object.keys(response.data)
+        setTlmNames(() => tlmNames)
+    }
+    
+    useEffect(() => {
+        fetchTlmNames()
+    }, [])
     
     const plotTelemetry = async () => {
         console.log('Plot')
-        const response = await fetch('http://localhost:3000/api/fetch-tlm')
-        const data = await response.json()
-        setDate(() => data['DATE'])
-        setVoltage(() => data['PCDU_BAT_VOLTAGE'])
+        const response = await axios.get('http://localhost:3000/api/fetch-tlm')
+        const { DATE, ...otherData } = response.data
+        setDate(() => response.data['DATE'])
+        setOtherTlm(() => otherData)
     }
     return (
         <>
@@ -26,23 +38,41 @@ const GroundTest = () => {
                     <a>Back to Home</a>
                 </Link>
             </h2>
+            {
+                tlmNames.map((tlmName, index) => {
+                    return (
+                        <p key={ index }>{ tlmName }</p>
+                    )
+                })
+            }
             <button onClick={plotTelemetry}>Plot</button>
-            <Plot
-                data={[
-                    {
-                        x: date,
-                        y: voltage,
-                        type: 'scattergl',
-                        mode: 'lines+markers',
-                        marker: { color: 'red'}
-                    },
-                ]}
-                layout={{
-                    width: 500,
-                    height: 500,
-                    title: 'Tutorial Plot'
-                }}
-            />
+            <div>
+                {
+                    Object.keys(otherTlm).map((key, index) => {
+                        const colorLength = plotlyColors.length
+                        const color = plotlyColors[index % colorLength]
+                        return (
+                            <Plot
+                                key={ index }
+                                data={[
+                                    {
+                                        x: date,
+                                        y: otherTlm[key],
+                                        type: 'scattergl',
+                                        mode: 'lines+markers',
+                                        marker: { color: color }
+                                    }
+                                ]}
+                                layout={{
+                                    width: 500,
+                                    height: 500,
+                                    title: key 
+                                }}
+                            />
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
