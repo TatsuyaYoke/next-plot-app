c6d89344b9d8154737615cb31053c09dfa853bdd
Yoke <tayok@synspective.com>
2022-03-12 06:35:03 +0900

    search function

--- pages/plot/ground-test.js
+++ pages/plot/ground-test.js
@@ -4,21 +4,58 @@ import React, { useState, useEffect } from 'react'
 import Plot from 'react-plotly.js'
 import axios from 'axios'
 
+const products = [
+    "apple",
+    "banana",
+    "orange",
+    "cheese cake",
+    "banana cake",
+    "apple juice",
+    "orange juice"
+]
+
 const GroundTest = () => {
     const [date, setDate] = useState([])
     const [otherTlm, setOtherTlm] = useState({})
     const [tlmNames, setTlmNames] = useState([])
     const plotlyColors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52'] 
     
+    const [keyword, setKeyword] = useState("")
+    const [showLists, setShowLists] = useState(false)
+    const [filteredProducts, setFilteredProducts] = useState(products)
+
     const fetchTlmNames = async () => {
         const response = await axios.get('http://localhost:3000/api/fetch-tlm')
         const tlmNames = Object.keys(response.data)
         setTlmNames(() => tlmNames)
     }
     
+    const filterProducts = () => {
+        if (keyword === "") {
+            setFilteredProducts(products)
+            return
+        }
+      
+        const searchKeywords = keyword
+            .trim()
+            .toLowerCase()
+            .match(/[^\s]+/g);
+
+        if (searchKeywords === null) {
+            setFilteredProducts(products)
+            return
+        }
+        const result = products.filter((product) =>
+            searchKeywords.every((kw) => product.toLowerCase().indexOf(kw) !== -1)
+        )
+      
+        setFilteredProducts(result.length ? result : ["No Item Found"])
+    }
+
     useEffect(() => {
-        fetchTlmNames()
-    }, [])
+        // fetchTlmNames()
+        filterProducts()
+    }, [keyword])
     
     const plotTelemetry = async () => {
         console.log('Plot')
@@ -38,6 +75,17 @@ const GroundTest = () => {
                     <a>Back to Home</a>
                 </Link>
             </h2>
+            <input 
+                onChange={e => setKeyword(e.target.value)}
+                onClick={() => setShowLists(true)}
+            />
+            { 
+                showLists && filteredProducts.map((v, i) => {
+                    return (
+                        <p key={ i }>{ v }</p>
+                    )
+                })
+            }
             {
                 tlmNames.map((tlmName, index) => {
                     return (
